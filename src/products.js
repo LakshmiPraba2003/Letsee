1e03779b40e4c61fa76d40b1dfcb2798fef0c045
LakshmiPraba2003 <108710753+LakshmiPraba2003@users.noreply.github.com>
2023-04-05 15:25:17 +0530

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,51 @@
+import product1 from './assets/1.png';
+import product2 from './assets/2.png';
+import product3 from './assets/3.png';
+import product4 from './assets/4.png';
+import product5 from './assets/5.png';
+import product6 from './assets/6.png';
+import product7 from './assets/7.png';
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Gold Half Rim Rectangle Eyeglasses",
+        price:3200,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Brown Tortoise Full Rim Rectangle Eyeglasses",
+        price:3000,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Silver Full Rim Aviator Eyeglasses",
+        price:1700,
+        productImage: product3,
+    },
+    {
+        id: 1,
+        productName: "Brown Striped Full Rim Round Eyeglasses",
+        price:1000,
+        productImage: product4,
+    },
+    {
+        id: 1,
+        productName: "Purple Full Rim Round Eyeglasses",
+        price:1500,
+        productImage: product5,
+    },
+    {
+        id: 1,
+        productName: "Blue Full Rim Round Eyeglasses",
+        price:3100,
+        productImage: product6,
+    },
+    {
+        id: 1,
+        productName: "Black Full Rim Rectangle Eyeglasses",
+        price:2000,
+        productImage: product7,
+    },
+]
